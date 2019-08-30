25fe6bd2c37c5817fbd4255bce0ff248d55bed14
Matthew Voss <voss.matthew@gmail.com>
2019-08-30 09:16:37 -0500

    update to es6

--- index.js
+++ index.js
@@ -1,113 +1,106 @@
-var fs = require('fs')
-var path = require('path')
-var marked = require('marked')
-var jsYaml = require('js-yaml')
-var requireResolve = require('require-resolve')
+const fs = require('fs')
+const path = require('path')
+const marked = require('marked')
+const jsYaml = require('js-yaml')
+const requireResolve = require('require-resolve')
 
-function mdToJs(md, convert) {
-    var props = {}
-    var contents = md
+const mdToJs = (md, convert) => {
+  const props = {}
+  let contents = md
 
-    if (md.slice(0, 4) === '---\n') {
-        var index = md.indexOf('---\n', 5)
+  if (md.slice(0, 4) === '---\n') {
+    const index = md.indexOf('---\n', 5)
 
-        if (index > 0) {
-            var yamlString = md.slice(0, index).substring(4)
-
-            props = jsYaml.safeLoad(yamlString)
-
-            contents = md.substring(index + 4)
-        }
+    if (index > 0) {
+      const yamlString = md.slice(0, index).substring(4)
+      props = jsYaml.safeLoad(yamlString)
+      contents = md.substring(index + 4)
     }
+  }
 
-    if (convert) {
-        contents = marked(contents)
-    }
+  if (convert) {
+    contents = marked(contents)
+  }
 
-    return Object.assign({}, props, {contents: contents})
+  return Object.assign({}, props, { contents: contents })
 }
 
-function toTree(t, obj) {
-    var props = []
+const toTree = (t, obj) => {
+  const props = []
 
-    for (var key in obj) {
-        var val = obj[key]
+  for (const key in obj) {
+    let val = obj[key]
 
-        if (val === null) {
-            val = t.nullLiteral()
-        } else {
-            var type = typeof(val)
+    if (val === null) {
+      val = t.nullLiteral()
+    } else {
+      const type = typeof(val)
 
-            if (type ===  'undefined') {
-                continue
-            }
+      if (type ===  'undefined') {
+        continue
+      }
 
-            switch(type) {
-                case 'string':
-                    val = t.stringLiteral(val)
+      switch(type) {
+        case 'string':
+          val = t.stringLiteral(val)
+          break
 
-                    break
+        case 'number':
+          val = t.numericLiteral(val)
+          break
 
-                case 'number':
-                    val = t.numericLiteral(val)
+        case 'boolean':
+          val = t.booleanLiteral(val)
+          break
 
-                    break
-
-                case 'boolean':
-                    val = t.booleanLiteral(val)
-
-                    break
-
-                default:
-                    val = toTree(t, val)
-            }
-        }
-
-        props.push(t.objectProperty(t.stringLiteral(key), val))
+        default:
+          val = toTree(t, val)
+      }
     }
 
-    return t.objectExpression(props)
+    props.push(t.objectProperty(t.stringLiteral(key), val))
+  }
+
+  return t.objectExpression(props)
 }
 
-module.exports = function (babel) {
-    var t = babel.types
+module.exports = (babel) => {
+  const t = babel.types
 
-    return {
-        visitor: {
-            ImportDeclaration: {
-                // pretty much guessing what input paramters are called
+  return {
+    visitor: {
+      ImportDeclaration: {
+        // pretty much guessing what input paramters are called
 
-                exit: function(decl, file) {
-                    var node = decl.node
+        exit: (decl, file) => {
+          const node = decl.node
+          let pathname = node.source.value
 
-                    var pathname = node.source.value
+          if (pathname.endsWith('.md')) {
+            // everything you see here is a complete guesswork but
+            // that is what you get without proper documentation -
+            // #babel6
 
-                    if (pathname.endsWith('.md')) {
-                        // everything you see here is a complete guesswork but
-                        // that is what you get without proper documentation -
-                        // #babel6
+            const mod = requireResolve(pathname, path.resolve(file.file.opts.filename))
+            const id = t.identifier(node.specifiers[0].local.name)
+            const value = toTree(t, mdToJs(fs.readFileSync(mod.src).toString(), true)) // due to bugs we cannot use t.valueToNode
 
-                        var mod = requireResolve(pathname, path.resolve(file.file.opts.filename))
-                        var id = t.identifier(node.specifiers[0].local.name)
-                        var value = toTree(t, mdToJs(fs.readFileSync(mod.src).toString(), true)) // due to bugs we cannot use t.valueToNode
+            decl.replaceWith(t.constiableDeclaration('const', [t.constiableDeclarator(id, value)]))
+          } else if (pathname.endsWith('.md!')) {
+            // everything you see here is a complete guesswork but
+            // that is what you get without proper documentation -
+            // #babel6
 
-                        decl.replaceWith(t.variableDeclaration('var', [t.variableDeclarator(id, value)]))
-                    } else
-                    if (pathname.endsWith('.md!')) {
-                        // everything you see here is a complete guesswork but
-                        // that is what you get without proper documentation -
-                        // #babel6
+            pathname = pathname.slice(0, -1)
 
-                        pathname = pathname.slice(0, -1)
+            const mod = requireResolve(pathname, path.resolve(file.file.opts.filename))
+            const id = t.identifier(node.specifiers[0].local.name)
+            const value = toTree(t, mdToJs(fs.readFileSync(mod.src).toString(), false)) // due to bugs we cannot use t.valueToNode
 
-                        var mod = requireResolve(pathname, path.resolve(file.file.opts.filename))
-                        var id = t.identifier(node.specifiers[0].local.name)
-                        var value = toTree(t, mdToJs(fs.readFileSync(mod.src).toString(), false)) // due to bugs we cannot use t.valueToNode
-
-                        decl.replaceWith(t.variableDeclaration('var', [t.variableDeclarator(id, value)]))
-                    }
-                }
-            }
+            decl.replaceWith(t.constiableDeclaration('const', [t.constiableDeclarator(id, value)]))
+          }
         }
+      }
     }
+  }
 }
